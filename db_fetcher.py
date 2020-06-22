4702c4bcf3c0a71b009bba807dcb23cc9768ee84
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2020-06-22 12:18:25 +0300

    Update db_fetcher.py

--- db_fetcher.py
+++ db_fetcher.py
@@ -1,46 +1,42 @@
 import mysql.connector
 
+class Db_connect:
+
+    def __init__(self, user='*****', password='*****', database='*****'):
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def __enter__(self):
+        self.con = mysql.connector.connect(
+            user=self.user, 
+            password=self.password, 
+            database=self.database
+        )
+        self.curs = self.con.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.curs.close()
+        self.con.close()
+        
+
 def get_value(condition): 
-    # receives a string argument equal to some primary key in db table and returns its description from another column
+    # receives a string argument equal to some primary key in db table
+    # and returns its description from another column
     
-    config = {
-        'user': '***', 
-        'password': '***', 
-        'database': '***'
-    }
-
-    con = mysql.connector.connect(**config)
-    curs = con.cursor()
-
-    stmt = 'SELECT con_description FROM conditions WHERE con_name = "' + condition + '"'
-
-    curs.execute(stmt)
-    result = curs.fetchall()
-
-    return result[0][0]
-
-    curs.close()
-    con.close()
+    with Db_connect() as manager:
+        stmt = 'SELECT con_description FROM conditions WHERE con_name = "{}"'.format(condition)
+        manager.curs.execute(stmt)
+        result = manager.curs.fetchall()
+        return result[0][0]
 
 def get_pkey(): 
     # forms a list of primary key values from database
 
-    config = {
-        'user': '***', 
-        'password': '***', 
-        'database': '***'
-    }
-
-    con = mysql.connector.connect(**config)
-    curs = con.cursor()
-
-    stmt = 'SELECT con_name FROM conditions'
-
-    curs.execute(stmt)
-    result = curs.fetchall()
-
-    pkey_list = [i[0] for i in result]
-    return pkey_list
-
-    curs.close()
-    con.close()
+    with Db_connect() as manager:
+        stmt = 'SELECT con_name FROM conditions'
+        manager.curs.execute(stmt)
+        result = manager.curs.fetchall()
+        pkey_list = [i[0] for i in result]
+        return pkey_list
